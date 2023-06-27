9b6f213393e3b5c5c91c80f9c42a1fb75e4b2e10
Lance Turbes <instinct_truster@protonmail.com>
2023-06-27 13:06:36 -0700

    complete advanced-functions part 1

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,45 @@
+// Steps: 1. Loop through array. 2. Create some HTML code for each todo. 3. Put the HTML on web page.
+
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "2022-12-22",
+  },
+  { name: "wash dishes", dueDate: "2022-12-22" },
+];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  todoList.forEach(function (todoObject, index) {
+    const { name, dueDate } = todoObject;
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+    todoList.splice(${index}, 1);
+    renderTodoList();
+    " class="delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
+  });
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector(".js-due-date-input");
+  const dueDate = dateInputElement.value;
+
+  // todoList.push({ name: name, dueDate: dueDate });
+  todoList.push({ name, dueDate }); // When the "property name" and "variable name" are the same, Objects have a "shorthand property syntax" which does the same thing as commented out code above.
+
+  inputElement.value = ""; // clears input textbox.
+
+  renderTodoList();
+}
